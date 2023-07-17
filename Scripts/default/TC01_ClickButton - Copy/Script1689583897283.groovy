baad42c59ead9e5d90d4d294c64427a7a31ab754
man.hua <man.hua@10.10.32.46>
2023-07-17 15:53:32 +0700

    us/github action for bitbucket migration after fixing bitbucket_pw

---
+++ Scripts/default/TC01_ClickButton - Copy/Script1689583897283.groovy
@@ -0,0 +1,33 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.testobject.SelectorMethod as SelectorMethod
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject
+import com.kms.katalon.core.util.KeywordUtil
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+TestObject healthcareLabel = new TestObject()
+
+healthcareLabel.setSelectorValue(SelectorMethod.XPATH, '//h1[text()=\'CURA Healthcare Service\']')
+
+healthcareLabel.setSelectorMethod(SelectorMethod.XPATH)
+
+WS.verifyMatch(WebUI.getText(healthcareLabel), 'CURA Healthcare Service', false)
+
+KeywordUtil.logInfo("Healthcare label: " + WebUI.getText(healthcareLabel))
+
+WebUI.click(findTestObject('Object Repository/Page_CURA Healthcare Service/a_Make Appointment'), FailureHandling.CONTINUE_ON_FAILURE)
+
