8bb839cebaf0c28e93c978b8819078f715f27395
manhuakatalon <man.hua@katalon.com>
2024-09-12 14:27:43 +0700

    add ts

---
+++ Test Suites/Test suite with timeout 650s.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test suite with timeout 650s</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>3299492b-abce-43ab-91ea-965093a12dd5</testSuiteGuid>
+   <testCaseLink>
+      <guid>6bc22bc9-c3b0-4f91-8347-d80482cc801c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/WebUI Keyword/TC_Timeout_650s_ClickButton</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
